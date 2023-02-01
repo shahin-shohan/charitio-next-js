a5dd66d7ab1851b9182de22cd176568fdb0fdd3c
shahin-shohan <56084608+shahin-shohan@users.noreply.github.com>
2023-02-02 00:54:37 +0600

    add new

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import 'photoswipe/dist/photoswipe.css'
+import '../styles/sass/style.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
